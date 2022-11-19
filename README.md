4e897ba4d9d1de12d505de94e4b40d4cf4f22b9a
yeyinghao <1113531030@qq.com>
2022-11-19 22:20:36 +0800

    初始化项目

--- README.md
+++
@@ -1,1 +0,0 @@
-# designmode
---
+++ src/main/java/com/yeyh/designmode/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.yeyh.designmode.service;
+
+/**
+ * @author yeyinghao
+ * @version 1.0.0
+ * @description:
+ * @date: 2022/11/19 22:05
+ */
+public interface OrderService {
+}
