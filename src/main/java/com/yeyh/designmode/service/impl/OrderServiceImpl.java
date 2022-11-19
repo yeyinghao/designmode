a3f118409c37294ba1fcc0ef56b74e07024d4e69
yeyinghao <1113531030@qq.com>
2022-11-19 22:16:10 +0800

    初始化项目

---
+++ src/main/java/com/yeyh/designmode/service/impl/OrderServiceImpl.java
@@ -0,0 +1,14 @@
+package com.yeyh.designmode.service.impl;
+
+import com.yeyh.designmode.service.OrderService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author yeyinghao
+ * @version 1.0.0
+ * @description:
+ * @date: 2022/11/19 22:06
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+}
